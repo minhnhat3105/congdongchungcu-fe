9329a494d135d6a70bcdf4cb92e438b30eb0ab72
daddy <minhnhat3105@gmail.com>
2021-09-27 16:59:39 +0700

    first-commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,55 @@
 import Vue from 'vue'
+import './pollyfills'
+import VueRouter from 'vue-router'
+import VueRouterPrefetch from 'vue-router-prefetch'
+import VueNotify from 'vue-notifyjs'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// Plugins
+import GlobalComponents from './globalComponents'
+import GlobalDirectives from './globalDirectives'
+import SideBar from './components/UIComponents/SidebarPlugin'
+import initProgress from './progressbar';
 
+// router setup
+import routes from './routes/routes'
+
+// library imports
+
+import './assets/sass/paper-dashboard.scss'
+import './assets/sass/demo.scss'
+
+import sidebarLinks from './sidebarLinks'
+import './registerServiceWorker'
+// plugin setup
+Vue.use(VueRouter)
+Vue.use(VueRouterPrefetch)
+Vue.use(GlobalDirectives)
+Vue.use(GlobalComponents)
+Vue.use(VueNotify)
+Vue.use(SideBar, {sidebarLinks: sidebarLinks})
+locale.use(lang)
+
+// configure router
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkActiveClass: 'active',
+  scrollBehavior: (to) => {
+    if (to.hash) {
+      return {selector: to.hash}
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+})
+
+initProgress(router);
+
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  router
+})
